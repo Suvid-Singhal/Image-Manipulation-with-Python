ff42800cf8c46c1078072b0e8ecc9966cdd05753
Suvid-Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2020-01-08 21:06:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,32 +9,14 @@ images = os.listdir()
 for Images in images:
     with Image.open(Images) as img:
         width, height = img.size
-        if (width > 400 and height > 400) and width > height:
-            junk = width - 400
-            if junk >= height:
-            	print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
-            	print("Image name: "+Images)
-            	new_width = width
-            	new_height = height
-            else:
-	            new_width = width - junk
-	            new_height = height - junk
-        elif (width > 400 and height > 400) and height > width:
-        	junk = height - 400
-        	if junk >= width:
-        		print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
-        		print("Image name: "+Images)
-        		new_width = width
-        		new_height = height
-        	else:
-        		new_width = width - junk
-        		new_height = height - junk
+        if width > height:
+            new_width  = 400
+            new_height = round(new_width * height / width)
         else:
-        	print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
-        	print("Image name: "+Images)
-        	new_height = height
-        	new_width = width
+            new_height = 400
+            new_width = round(new_height * width / height)
         img = img.resize((new_width, new_height), Image.ANTIALIAS)
+
         os.chdir(os.getcwd()+"/../"+folder+"_processed")
         img.save(os.getcwd()+"/"+Images+".jpeg", quality=12, optimize=True)
         os.chdir(os.getcwd()+"/../"+folder)
