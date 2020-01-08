5ec0f2435cbdc19b915326e2863dd51e6cda32ed
Suvid-Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2020-01-08 18:20:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,15 +9,32 @@ images = os.listdir()
 for Images in images:
     with Image.open(Images) as img:
         width, height = img.size
-        if width > 400 and width > height:
+        if (width > 400 and height > 400) and width > height:
             junk = width - 400
-            new_width = width - junk
-            new_height = height - junk
+            if junk >= height:
+            	print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
+            	print("Image name: "+Images)
+            	new_width = width
+            	new_height = height
+            else:
+	            new_width = width - junk
+	            new_height = height - junk
+        elif (width > 400 and height > 400) and height > width:
+        	junk = height - 400
+        	if junk >= width:
+        		print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
+        		print("Image name: "+Images)
+        		new_width = width
+        		new_height = height
+        	else:
+        		new_width = width - junk
+        		new_height = height - junk
         else:
-            junk = height - 400
-            new_height = height - junk
-            new_width = width - junk
-        img.thumbnail((width, height), Image.ANTIALIAS)
+        	print("Oops... Image cannot be resized while maintaining Aspect Ratio...")
+        	print("Image name: "+Images)
+        	new_height = height
+        	new_width = width
+        img = img.resize((new_width, new_height), Image.ANTIALIAS)
         os.chdir(os.getcwd()+"/../"+folder+"_processed")
         img.save(os.getcwd()+"/"+Images+".jpeg", quality=12, optimize=True)
         os.chdir(os.getcwd()+"/../"+folder)
