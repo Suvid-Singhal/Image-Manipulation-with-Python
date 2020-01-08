0b1487b091df81e2d6cae1113205f46bebc0240e
Suvid-Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2020-01-08 21:20:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ os.chdir(os.getcwd()+"/"+folder)
 images = os.listdir()
 
 for Images in images:
+    path = os.getcwd()+"/"+Images
+    filename, file_extension = os.path.splitext(path)
     with Image.open(Images) as img:
         width, height = img.size
         if width > height:
@@ -18,5 +20,5 @@ for Images in images:
         img = img.resize((new_width, new_height), Image.ANTIALIAS)
 
         os.chdir(os.getcwd()+"/../"+folder+"_processed")
-        img.save(os.getcwd()+"/"+Images+".jpeg", quality=12, optimize=True)
+        img.save(os.getcwd()+"/"+Images, quality=12, optimize=True)
         os.chdir(os.getcwd()+"/../"+folder)
